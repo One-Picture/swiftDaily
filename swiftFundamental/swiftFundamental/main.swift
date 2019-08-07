bfe8aff69f9900c85d194b910e2449e430806963
yihua.hu <yihua.hu@yintech.cn>
2019-08-07 17:54:19 +0800

    create swift project

---
+++ swiftFundamental/swiftFundamental/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftFundamental
+//
+//  Created by yihua on 2019/8/7.
+//  Copyright © 2019 yihua. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
